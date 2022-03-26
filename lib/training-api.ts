a31901b19563ce6ded532fc6b07f6031e4705244
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2022-03-26 11:50:07 +0100

    training API integration

---
+++ lib/training-api.ts
@@ -0,0 +1,29 @@
+export type TaskScore = {
+  score: number;
+  name: string;
+  title: string;
+};
+
+export type UserInfo = {
+  username: string;
+  success: number;
+  scores: TaskScore[];
+};
+
+export async function getUserInfo(username: string): Promise<UserInfo | null> {
+  try {
+    const body = { action: "get", username };
+    const req = await fetch("https://training.olinfo.it/api/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+      credentials: "include",
+    });
+    if (req.status !== 200) return null;
+    return await req.json();
+  } catch {
+    return null;
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,34 @@
 import { Loading } from "components/loading/Loading";
 import { Login } from "components/login/Login";
 import { useLoggedUser } from "lib/auth";
+import { getUserInfo, UserInfo } from "lib/training-api";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const user = useLoggedUser();
-  if (user === undefined) return <Loading />;
-  if (user === null) return <Login />;
+  const [userInfo, setUserInfo] = useState<UserInfo | null | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    if (!user) return;
+    console.log("> user", user);
+    getUserInfo(user.username).then((info) => setUserInfo(info));
+  }, [user]);
+
+  if (user === undefined || userInfo === undefined) return <Loading />;
+  if (user === null || userInfo === null) return <Login />;
 
   return (
-    <p>
-      Ciao {user.firstName} {user.lastName}
-    </p>
+    <div>
+      <p>
+        Ciao {user.firstName} {user.lastName}
+      </p>
+      {userInfo.scores.map((s) => (
+        <p key={s.name}>
+          {s.title} ({s.name}) -- {s.score}
+        </p>
+      ))}
+    </div>
   );
 }
