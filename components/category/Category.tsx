4fdc7baefb0e057194e4ae55d92d93313ceaa6ba
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2022-03-27 22:10:29 +0200

    Add resources markdown

--- components/category/Category.tsx
+++ components/category/Category.tsx
@@ -1,10 +1,21 @@
 import { CategoryBadge } from "lib/badges";
 import styles from "./Category.module.scss";
+import ReactMarkdown from "react-markdown";
 
 export function Category({ badge }: { badge: CategoryBadge }) {
   return (
     <div className={styles.category}>
-      <h2>{badge.node.title}</h2>
+      <h2 className={styles.title}>{badge.node.title}</h2>
+      <ReactMarkdown
+        components={{
+          h1: "h3",
+          h2: "h4",
+          h3: "h5",
+          h4: "h6",
+        }}
+      >
+        {badge.node.resources_md}
+      </ReactMarkdown>
     </div>
   );
 }
