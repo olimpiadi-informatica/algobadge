a9026f41ca018b9ca0ad44906d31a10c5eb3c9f1
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2022-03-27 23:30:44 +0200

    Revert "Use next/image for the loading spinner"
    next/image is not supported with `next export`
    
    This reverts commit 1cdb45209ce200d2caf9e34cc1a7add3c5eebbb9.

--- components/loading/Loading.tsx
+++ components/loading/Loading.tsx
@@ -1,12 +1,9 @@
 import styles from "./Loading.module.scss";
-import Image from "next/image";
 
 export function Loading() {
   return (
     <div className={styles.loading}>
-      <div>
-        <Image src="/loading.gif" alt="Loading..." width={100} height={100} />
-      </div>
+      <img src="/loading.gif" alt="Loading..." />
       caricamento...
     </div>
   );
